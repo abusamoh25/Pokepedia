64880282493a623fd000d179d10f82971f729a52
abusamoh25 <55249694+abusamoh25@users.noreply.github.com>
2020-09-13 20:52:13 -0500

    Cleaned up app.js

--- Pokepedia/app.js
+++ Pokepedia/app.js
@@ -1,15 +1,10 @@
 //jshint esversion: 6
-// require("dotenv").config()
+
 const request = require("request");
 const https = require("https");
 const express = require("express")
 const bodyParser = require("body-parser")
 const path = require("path");
-// const ejs = require("ejs")
-// const mongoose = require("mongoose")
-// const session = require("express-session")
-// const passport = require("passport")
-// const passportLocalMongoose = require("passport-local-mongoose");
 const { response } = require("express");
 
 const app = express();
@@ -17,55 +12,10 @@ const app = express();
 app.use("/static", express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-
-// app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({
     extended: true
 }))
 
-// app.use(session({
-//     secret: process.env.SECRET,
-//     resave: false,
-//     saveUninitialized: false
-// }))
-
-// app.use(passport.initialize())
-// app.use(passport.session())
-
-// mongoose.connect(
-//     `mongodb+srv://EricNwagwu:${process.env.DB_PASS}@cluster0-ogxz8.mongodb.net/pokemonDB`,
-//     { useNewUrlParser: true, useUnifiedTopology: true }
-// )
-// mongoose.set("useCreateIndex", true);
-
-// const userSchema = new mongoose.Schema({
-//     username: String,
-//     password: String,
-//     favoritePokemon: [String], 
-// })
-
-// userSchema.plugin(passportLocalMongoose)
-
-// const User = new mongoose.model("User", userSchema)
-
-// passport.use(User.createStrategy());
-
-// passport.serializeUser(User.serializeUser());
-// passport.deserializeUser(User.deserializeUser());
-
-
-// app.get("/", function (req, res) {
-//     res.render("home")
-// })
-
-// app.get("/register",function(req,res){
-//     res.render("register")
-// })
-
-// app.get("/login",function(req,res){
-//     res.render("login")
-// })
 
 app.get("/", function(req, res) {
 
@@ -126,57 +76,6 @@ app.post("/pokemon", function (req, res) {
     })
 
 })
-
-
-// app.post("/register", function (req, res) {
-//     User.register({ username: req.body.username }, req.body.password, function (err, user) {
-//         if (err) {
-//             console.log(err)
-//             res.redirect("/register")
-//         } else {
-//             passport.authenticate("local")(req, res, function () {
-//                 user.favoritePokemon = []
-//                 user.save()
-//                 res.redirect("/favorite")
-//             });
-//         }
-//     })
-// })
-
-
-
-// app.post("/login", function (req, res) {
-//     const user = new User({
-//         username: req.body.username,
-//         password: req.body.password
-//     })
-
-//     req.login(user, function (err) {
-//         if (err) {
-//             console.log(err)
-//             res.redirect("/login")
-//         } else {
-//             passport.authenticate("local")(req, res, function () {
-//                 res.redirect("/favorite")
-//                 user.favoritePokemon
-//             })
-//         }
-//     })
-// })
-
-
-// app.get("/favorite",function(req,res){
-//     if (req.isAuthenticated()) {
-//         res.render("favorite",{currentUser: req.user})
-//     } else {
-//         res.redirect("/login")
-//     }
-// })
-
-// app.get("/logout", function (req, res) {
-//     req.logout();
-//     res.redirect("/");
-// })
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000")
